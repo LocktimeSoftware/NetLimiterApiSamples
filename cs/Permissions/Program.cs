04fa2217b68c7abf17fed96807bad01784fec1ec
czendoo <czendoo@proton.me>
2024-09-20 09:36:06 +0200

    Permissions sample added

---
+++ cs/Permissions/Program.cs
@@ -0,0 +1,39 @@
+﻿using NetLimiter.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLApiSamples
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using (NLClient client = new NLClient())
+            {
+                // Connect to local NetLimiter service
+                client.Connect();
+
+                try
+                {
+                    var table = client.GetAccessTable().ToList();
+
+                    table.Add(new NetLimiter.Service.Api.AccessTableRow()
+                    {
+                        Name = "pokus",
+                        AllowedRights = NetLimiter.Service.Security.Rights.Monitor,
+                        DeniedRights = NetLimiter.Service.Security.Rights.Control
+                    });
+
+                    client.SetAccessTable(table);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+            }
+        }
+    }
+}
